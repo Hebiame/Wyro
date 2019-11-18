dccfd427228eab065f06f565cf7a84a918d0b709
Hebiame <hebiame@gmail.com>
2019-11-18 23:42:10 +0100

    Set webdriver version

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -4,7 +4,9 @@ config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
     args: ['--headless', '--no-sandbox']
-  }
+  },
+  chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_77.0.3865.75',
+  directConnect: true
 };
 
 exports.config = config;
